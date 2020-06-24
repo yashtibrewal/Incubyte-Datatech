f443d3f7bfd26b0f092fc6d1b6173a45f9fcfe68
yashtibrewal <yashkush.tibrewal@gmail.com>
2020-06-24 16:01:10 +0530

    Requirement 12 Completed, Tests Passed

--- Assignment/src/test/StringCalculatorTest.java
+++ Assignment/src/test/StringCalculatorTest.java
@@ -80,6 +80,9 @@ class StringCalculatorTest {
 		result = stringCalulatorObject.add("//[.][%]1%2.3");
 		assertEquals(6, result);
 		
+		stringCalulatorObject= new StringCalulator();
+		result = stringCalulatorObject.add("//[..][%%][***]1%%2..3***1001..1000");
+		assertEquals(1006, result);
 		
 		System.out.println("add() function was called "+stringCalulatorObject.getCalledCount()+" times");
 		

7f8ce4f4dc84b50dff3b3cd366f14ac5044d11b9
yashtibrewal <yashkush.tibrewal@gmail.com>
2020-06-24 13:18:13 +0530

    Requirement 6 Completed, Tests Passed

--- Assignment/src/dev/StringCalulator.java
+++ Assignment/src/dev/StringCalulator.java
@@ -4,64 +4,86 @@ import java.util.ArrayList;
 
 public class StringCalulator {
 	ArrayList<Character> delimiters;
+	ArrayList<Integer> negatives;
+
 	public StringCalulator() {
 		delimiters = new ArrayList<Character>(2);
+		negatives = new ArrayList<Integer>(1);
 		addDefaultDelimeters();
 	}
-	
+
 	public void addDefaultDelimeters() {
 		delimiters.add(',');
 		delimiters.add('\n');
 	}
-	
-	public int add(String input) throws Exception {
 
+	public int add(String input) throws Exception {
+		boolean hasNegatives = false;
 		int sum = 0;
-		
-		
-		if(input.length()==0) {
+
+		if (input.length() == 0) {
 			return 0;
 		}
-		
+
 		// handling delimiter change
-		if(input.length()>=3)
-			if(input.charAt(0)=='/' && input.charAt(1)=='/') {
+		if (input.length() >= 3)
+			if (input.charAt(0) == '/' && input.charAt(1) == '/') {
 				addDelimeter(input.charAt(2));
-				input = input.substring(3);  // removing the "//delimiter"
+				input = input.substring(3); // removing the "//delimiter"
 			}
-		
-		
-		String num = ""; //to hold 1 number at a time
-		for(int i=0;i<input.length();i++) { // processing every character
-			
+
+		String num = ""; // to hold 1 number at a time
+		for (int i = 0; i < input.length(); i++) { // processing every character
+
 			char ch = input.charAt(i);
 			// checking for the delimiter
-			if(!isDelimeter(ch))  
-				num+=ch;
-			//logic to feed one number
-			if(isDelimeter(ch)) {
-				if(Integer.parseInt(num)<0)
-					throw new Exception("negatives not allowed "+num);
-				sum+=Integer.parseInt(num);
+			if (!isDelimeter(ch))
+				num += ch;
+			// logic to feed one number
+			if (isDelimeter(ch)) {
+				if (checkNegative(num, false)) {
+					if (!hasNegatives)
+						hasNegatives = true;
+					checkNegative(num, true);
+				}
+				sum += Integer.parseInt(num);
 				// handling negative numbers
-				num="";
+				num = "";
 			}
-			
-			if(i==input.length()-1) { // to add the last number
-				sum+=Integer.parseInt(num);
+
+			if (i == input.length() - 1) { // to add the last number
+				checkNegative(num, true);
+				sum += Integer.parseInt(num);
 			}
-			
+
 		}
-		
+		if (hasNegatives)
+			throw new Exception("negatives not allowed " + negatives);
 		return sum;
 	}
-	
+
+	/**
+	 * @param num: parameter to be checked if its a negative number and return
+	 *             result
+	 * @param add: parameter to control whether to add the number to the list of
+	 *             integers
+	 * @return boolean
+	 */
+	public boolean checkNegative(String num, boolean add) {
+		if (Integer.parseInt(num) < 0) {
+			if (add)
+				negatives.add(Integer.parseInt(num));
+			return true;
+		}
+		return false;
+	}
+
 	public boolean isDelimeter(char ch) {
 		return delimiters.contains(ch);
 	}
-	
+
 	public void addDelimeter(char ch) {
 		delimiters.add(ch);
 	}
-	
+
 }
--- Assignment/src/test/StringCalculatorTest.java
+++ Assignment/src/test/StringCalculatorTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import dev.StringCalulator;
-
 
 class StringCalculatorTest {
 
@@ -21,37 +21,62 @@ class StringCalculatorTest {
 	@Test
 	final void testAdd() throws Exception {
 		int result;
-		StringCalulator stringCalulatorObject = new StringCalulator();
+		String expectedMessage, actualMessage;
+
+		StringCalulator stringCalulatorObject;
+		
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("");
 		assertEquals(0, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("1");
 		assertEquals(1, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("1,2");
 		assertEquals(3, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("1,2,3");
 		assertEquals(6, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("2,45,1234,0,0,0,0,0,1,2,99");
 		assertEquals(1383, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("1\n2,3\n99");
 		assertEquals(105, result);
+
+		stringCalulatorObject= new StringCalulator();
 		result = stringCalulatorObject.add("//;1;2;3");
 		assertEquals(6, result);
-		Exception exception = assertThrows(Exception.class, 
-				()->{
-						stringCalulatorObject.add("1,-2,3");
-					}
-		);
-	    String expectedMessage = "negatives not allowed -2";
-	    String actualMessage = exception.getMessage();
-	    assertTrue(actualMessage.contains(expectedMessage));
-		
+
+		StringCalulator stringCalulatorObjectR5= new StringCalulator();
+		Exception exception = assertThrows(Exception.class,()->{
+				stringCalulatorObjectR5.add("1,-2,3");
+		});
+		expectedMessage = "negatives not allowed [-2]";
+		actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
+
+		StringCalulator stringCalulatorObjectR6= new StringCalulator();
+		exception = assertThrows(Exception.class, () -> {
+			stringCalulatorObjectR6.add("1,-2,-3");
+		});
+		expectedMessage = "negatives not allowed [-2, -3]";
+		actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
+
 	}
-	
+
 	@Test
 	final void testIsDelimeter() {
 		StringCalulator stringCalulatorObject = new StringCalulator();
 		assertEquals(true, stringCalulatorObject.isDelimeter('\n'));
 		assertEquals(true, stringCalulatorObject.isDelimeter(','));
+		assertEquals(false, stringCalulatorObject.isDelimeter(';'));
 	}
-	
+
 }
